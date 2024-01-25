bff0b8ea022e3c537c4cb8ca8555da022bd146a6
Michael Leach <mike@purple.ai>
2024-01-25 15:58:01 +0000

    Fixes D1P2

--- src/AOC/D1/P2/solve.php
+++ src/AOC/D1/P2/solve.php
@@ -1,9 +1,12 @@
 <?php
 
+use AOC\D1\P1\CalibrationValueAggregator;
+use AOC\D1\P2\Part2ValueRetriever;
+use AOC\Helper\InputReader;
+
 require_once __DIR__ . '/../../../../vendor/autoload.php';
 
-use AOC\D1\P2\ElfGroup;
+$input = InputReader::fileToLines(__DIR__ . '/../input.txt');
+$total = (new CalibrationValueAggregator($input, new Part2ValueRetriever()))->getTotal();
 
-$answer = ElfGroup::fromInput(__DIR__ . '/../input.txt')->getCalorieCountTopElves(3);
-
-echo 'Part 2: ' . $answer . "\n";
+echo 'Part 2: ' . $total . "\n";
